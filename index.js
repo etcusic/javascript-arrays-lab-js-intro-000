a988c918f420db40d739b62d61f43ce042874307
Eli Cusic <tntitans31@gmail.com>
2020-06-04 17:54:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+var destructivelyAppendKitten = function (name){
+  kittens.push(name)
+  return kittens
+}
+
+var destructivelyPrependKitten = function (name){
+  kittens.unshift(name)
+  return kittens
+}
+
+var destructivelyRemoveLastKitten = function (name){
+  kittens.pop(name)
+  return kittens
+}
+
+var destructivelyRemoveFirstKitten = function (name){
+  kittens.shift(name)
+  return kittens
+}
+
+var appendKitten = function(name){
+  return kittens.concat(name)
+}
